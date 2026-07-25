7dcb8cf75d34375d73709ed3efc722757f39326c
tentateursss <Mativ4ik77@yandex.ru>
2026-07-25 13:10:03 +0300

    fix: добавлен полный путь для ViwsStats в запросах

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/stats/repository/HitRepository.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/stats/repository/HitRepository.java
@@ -12,14 +12,14 @@ import java.util.List;
 @Repository
 public interface HitRepository extends JpaRepository<Hit, Long> {
 
-    @Query("SELECT new ViewStats(h.app, h.uri, COUNT(h.id)) "
+    @Query("SELECT new ru.practicum.stats.dto.ViewStats(h.app, h.uri, COUNT(h.id)) "
             + "FROM Hit h "
             + "WHERE h.uri IN :uris "
             + "AND h.timestamp BETWEEN :start AND :end "
             + "GROUP BY h.app, h.uri")
     List<ViewStats> getHitsByDateLimits(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("SELECT new ViewStats(h.app, h.uri, COUNT(DISTINCT(h.ip))) "
+    @Query("SELECT new ru.practicum.stats.dto.ViewStats(h.app, h.uri, COUNT(DISTINCT(h.ip))) "
             + "FROM Hit h "
             + "WHERE h.uri IN :uris "
             + "AND h.timestamp BETWEEN :start AND :end "
