f617a68a38dcf95223b9a3283f38f60a4a47fef3
tentateursss <Mativ4ik77@yandex.ru>
2026-07-25 13:31:17 +0300

    fix: удалены неиспользуемые импорты

--- ewm-stats-service/stats-dto/src/main/java/ru/practicum/stats/dto/HitRequestDto.java
+++ ewm-stats-service/stats-dto/src/main/java/ru/practicum/stats/dto/HitRequestDto.java
@@ -1,13 +1,9 @@
 package ru.practicum.stats.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
