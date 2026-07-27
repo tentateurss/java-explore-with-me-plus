6cb3650d9f8d914b7b14e9c985eed3464d910f08
tentateursss <Mativ4ik77@yandex.ru>
2026-07-27 07:59:35 +0300

    Убрано лишнее

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/stats/util/DateTimeFormatters.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/stats/util/DateTimeFormatters.java
@@ -2,14 +2,13 @@ package ru.practicum.stats.util;
 
 import java.time.format.DateTimeFormatter;
 
-public class DateTimeFormatters  {
+public class DateTimeFormatters {
 
     public static final String PATTERN = "yyyy-MM-dd HH:mm:ss";
 
     public static final DateTimeFormatter STANDARD =
             DateTimeFormatter.ofPattern(PATTERN);
 
-    private DateTimeFormatters () {
-
+    private DateTimeFormatters() {
     }
 }
