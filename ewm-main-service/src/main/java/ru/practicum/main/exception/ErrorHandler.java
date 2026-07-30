11baaeedfb929224d1ff29042865d024d517d503
tentateursss <Mativ4ik77@yandex.ru>
2026-07-30 11:53:05 +0300

    Убраны ошибки CheckStyle

--- ewm-main-service/src/main/java/ru/practicum/main/exception/ErrorHandler.java
+++ ewm-main-service/src/main/java/ru/practicum/main/exception/ErrorHandler.java
@@ -2,14 +2,11 @@ package ru.practicum.main.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.main.dto.ApiError;
-
-import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
