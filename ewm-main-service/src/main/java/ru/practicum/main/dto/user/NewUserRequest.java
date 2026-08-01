566319c399d01737c42ee564b91eb491f15cc09b
anastasifade <anastasifade@gmail.com>
2026-08-01 18:00:34 +0400

    feat: add admin user controller tests

--- ewm-main-service/src/main/java/ru/practicum/main/dto/user/NewUserRequest.java
+++ ewm-main-service/src/main/java/ru/practicum/main/dto/user/NewUserRequest.java
@@ -3,9 +3,13 @@ package ru.practicum.main.dto.user;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NewUserRequest {
     @NotBlank(message = "Имя пользователя не может быть пустым.")
     @Size(min = 2, max = 250, message = "Имя пользователя должно быть в пределах [2-250] символов.")
