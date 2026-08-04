4587e564223ce122c6a515df8574434bc1f36671
tentateursss <Mativ4ik77@yandex.ru>
2026-08-04 18:40:04 +0300

    Убраны ошибки

--- ewm-main-service/src/main/java/ru/practicum/main/service/event/EventServiceImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/main/service/event/EventServiceImpl.java
@@ -30,7 +30,6 @@ import ru.practicum.stats.dto.ViewStats;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
