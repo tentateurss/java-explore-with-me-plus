625fe795b69e9f94499d7c3038daf994ffbc38a8
tentateursss <Mativ4ik77@yandex.ru>
2026-08-05 18:02:05 +0300

    Исправления по ревью

--- ewm-main-service/src/main/java/ru/practicum/main/service/request/ParticipationRequestServiceImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/main/service/request/ParticipationRequestServiceImpl.java
@@ -124,9 +124,10 @@ public class ParticipationRequestServiceImpl implements ParticipationRequestServ
             throw new ValidationException("Этот пользователь не инициатор");
         }
 
-        if (event.getParticipantLimit() != 0 && event.getParticipantLimit() <=
-                requestRepository.countByEventIdAndStatus(eventId, RequestStatus.CONFIRMED)) {
-            throw new ConflictException("Достигнут лимит заявок на участие");
+        List<ParticipationRequest> requests = requestRepository.findAllById(dto.getRequestIds());
+
+        if (requests.size() != dto.getRequestIds().size()) {
+            throw new NotFoundException("Одна или несколько заявок не найдены");
         }
 
         List<ParticipationRequest> confirmed = new ArrayList<>();
@@ -134,10 +135,7 @@ public class ParticipationRequestServiceImpl implements ParticipationRequestServ
         long confirmedCount = requestRepository.countByEventIdAndStatus(eventId, RequestStatus.CONFIRMED);
         int limit = event.getParticipantLimit();
 
-        for (Long requestId : dto.getRequestIds()) {
-            ParticipationRequest request = requestRepository.findById(requestId)
-                    .orElseThrow(() -> new NotFoundException("Заявка не найдена"));
-
+        for (ParticipationRequest request : requests) {
             if (request.getStatus() != RequestStatus.PENDING) {
                 throw new ConflictException("Статус заявки должен быть PENDING");
             }
